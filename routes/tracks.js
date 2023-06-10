5b5694ae43ff1b42da03f6cd6d5676dce44c9686
santiago_montoya <santiago.montoya@bluspot.us>
2023-06-09 23:40:59 -0500

    Added validators on routes

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,10 @@
 const express =  require("express")
 const { getItems, getItem, createItem } = require("../controllers/tracks")
 const router = express.Router()
+const { validatorCreateItem } = require('../validators/tracks')
 
 router.get("/", getItems)
 router.get("/:id", getItem)
-router.post("/", createItem)
-
-
+router.post("/", validatorCreateItem, createItem)
 
 module.exports = router
